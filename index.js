d04a2752b2aee1f4a49860857dbd7dbf641d56f1
purwar1997 <shubhampurwar511@gmail.com>
2023-02-17 15:36:23 +0530

    project ready for deployment

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 const express = require('express');
+const format = require('date-format');
 const app = express();
+
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.status(200).send('<h1>Welcome to Social App</h1>');
+});
+
+app.get('/api/v1/instagram', (req, res) => {
+  const instagram = {
+    username: 'shubhampurwar05',
+    followers: 50,
+    following: 100,
+    date: format('yyyy-MM-dd hh:mm:ss', new Date()),
+  };
+
+  res.status(200).json(instagram);
+});
+
+app.get('/api/v1/facebook', (req, res) => {
+  const facebook = {
+    username: 'shubhampurwar97',
+    friends: 50,
+    following: 30,
+    date: format('yyyy-MM-dd hh:mm:ss', new Date()),
+  };
+
+  res.status(200).json(facebook);
+});
+
+app.get('/api/v1/linkedin', (req, res) => {
+  const linkedin = {
+    username: 'shubhamdev_97',
+    followers: 50,
+    following: 40,
+    connections: 100,
+    date: format('yyyy-MM-dd hh:mm:ss', new Date()),
+  };
+
+  res.status(200).json(linkedin);
+});
+
+app.get('/api/v1/:token', (req, res) => {
+  const token = req.params.token;
+
+  res.status(400).json({
+    params: token,
+    message: `${token} is not a valid path`,
+  });
 });
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
